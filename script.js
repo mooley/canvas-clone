d41caea1a2544267d828424cbb29aa296ba2b335
Paulo Moore <pimoore@uc.cl>
2021-03-23 17:02:11 -0300

    More fixes to navbar. Now its functionality is identical to the original

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ document.getElementById('ham-menu').addEventListener('click', function () {
 	document.getElementById('ham-menu').classList.toggle('active');
 
 	// Display navbar menu when ham button is clicked
-	document.getElementById('navigation-menu').classList.toggle('show-menu');
 	document.getElementById('nav-container').classList.toggle('show-menu');
+
+	document.getElementById('navigation-menu').classList.toggle('show-items');
+});
+
+window.addEventListener('resize', function () {
+	let screenWidth = window.innerWidth;
+
+	if (screenWidth > 991) {
+		document
+			.getElementById('navigation-menu')
+			.classList.remove('show-items');
+
+		document.getElementById('ham-menu').classList.remove('active');
+
+		document.getElementById('nav-container').classList.remove('show-menu');
+	}
 });
