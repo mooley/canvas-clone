796259c1f344f4a6fa31c71a584d45792ed9ec92
Paulo Moore <pimoore@uc.cl>
2021-03-22 21:13:40 -0300

    Display navbar menu functional but not styled yet

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.getElementById('ham-menu').addEventListener('click', function () {
+	// Trigger animation when ham button is clicked
 	document.getElementById('ham-menu').classList.toggle('active');
+
+	// Display navbar menu when ham button is clicked
+	document.getElementById('navigation-menu').classList.toggle('show-menu');
+	document.getElementById('nav-container').classList.toggle('show-menu');
 });
