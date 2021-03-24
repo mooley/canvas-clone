7fff26e61be172548d86863b2becb41865d86778
Paulo Moore <pimoore@uc.cl>
2021-03-24 18:08:59 -0300

    Navbar ready

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ window.addEventListener('resize', function () {
 		document.getElementById('nav-container').classList.remove('show-menu');
 	}
 });
+
+let homeButton = document.getElementById('home-button');
+
+if (window.scrollY < 50) {
+	console.log(window.scrollY);
+	homeButton.classList.add('home-button-active');
+}
+
+window.addEventListener('scroll', function () {
+	console.log('scrolling');
+	if (window.scrollY < 50) {
+		window.setTimeout(function () {
+			homeButton.classList.add('home-button-active');
+		}, 500);
+	} else {
+		homeButton.classList.remove('home-button-active');
+	}
+});
